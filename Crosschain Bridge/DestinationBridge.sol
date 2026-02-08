babcfe1a769d06cdae4b0899bb524193202f9b7b
Jahanazib Khan <jahanzaibkhan3350@gmail.com>
2026-02-08 08:54:37 +0500

    Implement owner authorization in DestinationBridge
    Added owner authorization for unlock function.

--- Crosschain Bridge/DestinationBridge.sol
+++ Crosschain Bridge/DestinationBridge.sol
@@ -1,15 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "./extendedIERC20.sol";
+
+// This is the bridge contract which will be called by an external party. 
+
+import {IERC20} from "../Interface/extendedIERC20.sol";
 
 contract DestinationBridge{
     IERC20 private WZToken;
-    constructor(address adr) {
+    address private _owner;
+    constructor(address adr, address owner) {
         WZToken = IERC20(adr);
+        _owner = owner;
     }
     mapping (address => uint256) private _balances;
-    function unlock(address user, uint256 amount) public {
+    function unlock(address user, uint256 amount) public authorizedOnly {
         WZToken.mint(user, amount);
         _balances[user] += amount;
+    }
+    modifier authorizedOnly(){
+        require(msg.sender == _owner, "Authorized Only Function");
+        _;
     }
 }
